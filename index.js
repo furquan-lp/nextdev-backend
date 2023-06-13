cec84789465a979559242f8be283c6efb8cd1ee7
Syed F Ahmad <furquan@protonmail.com>
2023-06-13 12:31:42 +0530

    Imported version from package.json and added route for /versions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Carousel = require('./models/carousel');
 require('dotenv').config();
+const backendVersion = require('./package.json').version;
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json(), express.static('public'));
 
 app.get('/', (request, response) => response.sendFile('index.html', { root: path.join(__dirname, 'public') }));
 app.get('/db/carousel', (request, response) => Carousel.find({}).then(carousel => response.send(carousel)));
+app.get('/version', (request, response) => response.json({ version: backendVersion }));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
