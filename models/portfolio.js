ed3b395bbec94aafdc5510e8ee3d0a9f1f16f3e8
Syed F Ahmad <furquan@protonmail.com>
2023-06-16 00:48:24 +0530

    Added the model for portfolios

---
+++ models/portfolio.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+const cert = process.env.X509_CERT;
+
+(async () => {
+  await mongoose.connect(url, {
+    sslValidate: true,
+    tlsCertificateKeyFile: cert,
+    authMechanism: 'MONGODB-X509',
+    authSource: '$external'
+  }).catch(e => console.error('Error connecting to the Atlas backend.', e));
+})();
+
+const portfolioSchema = new mongoose.Schema({
+  image: String,
+  title: String,
+  text: String,
+  tags: [{
+    tech: String,
+    text: String,
+    color: String
+  }],
+  repo: String,
+  site: String
+})
+
+portfolioSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Portfolio', portfolioSchema);
